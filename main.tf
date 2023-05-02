992ed18d34146c9195d4a7d949da61328d86635c
sirisha517 <sai.sirifriends8790@gmail.com>
2023-05-02 14:52:55 +0530

    user component fix

--- main.tf
+++ main.tf
@@ -1,24 +1,26 @@
 resource "aws_elasticache_cluster" "elasticache" {
-  cluster_id           = "${var.env}-elasticache"
-  engine               = var.engine
-  engine_version       = var.engine_version
-  node_type            = var.node_type
-  num_cache_nodes      = var.num_cache_nodes
-  port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.main.name
-  security_group_ids   = [aws_security_group.main.id]
+  cluster_id         = "${var.env}-elasticache"
+  engine             = var.engine
+  node_type          = var.node_type
+  num_cache_nodes    = var.num_cache_nodes
+  engine_version     = var.engine_version
+  port               = 6379
+  subnet_group_name  = aws_elasticache_subnet_group.main.name
+  security_group_ids = [aws_security_group.main.id]
+
   tags = merge(
     var.tags,
-    { Name = "${var.env}-subnet-group"}
+    { Name = "${var.env}-elasticache" }
   )
 }
+
 resource "aws_security_group" "main" {
   name        = "elasticache-${var.env}"
   description = "elasticache-${var.env}"
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "SSH"
+    description = "ELASTICACHE"
     from_port   = 6379
     to_port     = 6379
     protocol    = "tcp"
@@ -38,12 +40,15 @@ resource "aws_security_group" "main" {
     { Name = "elasticache-${var.env}" }
   )
 }
+
+
 resource "aws_elasticache_subnet_group" "main" {
   name       = "${var.env}-rds"
   subnet_ids = var.subnet_ids
+
   tags = merge(
     var.tags,
-    { Name = "${var.env}-subnet-group"}
+    { Name = "${var.env}-subnet-group" }
   )
 }
 
@@ -52,8 +57,7 @@ output "redis" {
 }
 
 resource "aws_ssm_parameter" "elasticache_endpoint" {
-  name  = "${var.env}.elasticache_endpoint"
+  name  = "${var.env}.elasticache.endpoint"
   type  = "String"
   value = aws_elasticache_cluster.elasticache.cache_nodes[0].address
 }
-
